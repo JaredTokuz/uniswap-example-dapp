73afdf385d5eb23d2106a6183414c21bf000a33d
JaredTokuz <jtokuz@gmail.com>
2022-10-09 17:41:59 -0500

    info in the app env test

--- src/app-env.spec.ts
+++ src/app-env.spec.ts
@@ -1,4 +1,4 @@
-describe('AppController (e2e)', () => {
+describe('Makes sure the environment contains all Vars', () => {
   it('Check env', () => {
     expect(process.env.OPTIMISM_URL).toBeTruthy();
     expect(process.env.POLYGON_URL).toBeTruthy();
